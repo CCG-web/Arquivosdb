c82d43997fbc8acd3c45a8405a10cfb887ef35ca
David Marques <54871078+CCG-web@users.noreply.github.com>
2022-10-10 09:48:50 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,98 @@
+{
+  "comments": [
+    {
+      "body": "some comment",
+      "id": 1,
+      "postId": 1
+    }
+  ],
+  "products": [
+    {
+      "category": "electronics",
+      "description": "Mel de  JANDAÍRA DA AMAZÔNIA de excelente qualidade, pasteurizado e lacrado. Sabor levemente amargo, puramente medicinal. Sem nenhum contato externo. Mel 100% puro e de qualidade.",
+      "id": 1,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2021/07/mel-jandaria-amazonia-abelha-sem-ferrao-comprar.jpg",
+      "price": 30,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Mel DE ABELHA JATAÍ DO PARAMBU 60ML"
+    },
+    {
+      "category": "electronics",
+      "description": "Este produto trata-se de MEL 100% PURO, perfeito para harmonizar com alimentos e bebidas e ser tomado diariamente.",
+      "id": 2,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2021/10/Tezza_2021_10_18_162325791-510x510.jpg",
+      "price": 60,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Balde 3KG: Mel puro direto do produtor!"
+    },
+    {
+      "category": "electronics",
+      "description": "Nosso Mel é  CRU, o que significa que é envasado puro, direto do produtor/apiário, sem qualquer industrialização!",
+      "id": 3,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2020/08/silvestre-gourmet-mel_optimized-510x505.jpg",
+      "price": 60,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Mel 100% puro, direto do apiário."
+    },
+    {
+      "category": "electronics",
+      "description": "Este produto trata-se de FAVO DE MEL 100% PURO, perfeito para harmonizar com alimentos e pratos de sobremesas especiais",
+      "id": 4,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2020/08/mel-laranjeira-gourmet-450g-500g-sq_optimized-510x510.jpg",
+      "price": 20,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Mel 100% puro, direto do apiário."
+    },
+    {
+      "category": "electronics",
+      "description": "Este produto trata-se de FAVO DE MEL 100% PURO, perfeito para harmonizar com alimentos e pratos de sobremesas especiais",
+      "id": 5,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2019/08/favos-de-mel-puro-510x510.jpg",
+      "price": 25,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Favo de Mel"
+    },
+    {
+      "category": "fashion",
+      "description": "Própolis Verde sem Álcool – 20ml",
+      "id": 6,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2021/03/DSC_0288-2-510x510.jpg",
+      "price": 20,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Própolis Verde (20%) e água purificada (80%)."
+    },
+    {
+      "category": "fashion",
+      "description": "Uma meleira de vidro que irá facilitar sua vida!      Não precisa usar colher, pauzinho ou derramar mel",
+      "id": 7,
+      "image": "https://loja.mel.com.br/wp-content/uploads/2022/05/meleiras-1-510x510.jpg",
+      "price": 19,
+      "rating": {
+        "count": 120,
+        "rate": 3.9
+      },
+      "title": "Meleira de Vidro Comum 210ml (300g de Mel)"
+    }
+  ],
+  "profile": {
+    "name": "typicode"
+  }
+}
